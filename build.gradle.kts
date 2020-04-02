5d18755bf54b7a5f30118077eb6b0b60a9fcbccc
Jan Heinrich Reimer <heinrich@heinrichreimer.com>
2020-04-02 21:19:00 +0200

    Enable Maven publication, fix Kotlin compiler args

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
 plugins {
     kotlin("jvm") version "1.3.71"
     id("de.undercouch.download") version "4.0.2"
+    `maven-publish`
+    id("org.jetbrains.dokka") version "0.9.17"
 }
 
 group = "dev.reimer"
-version = "0.1.0"
+version = "0.1.1"
 
 repositories {
     mavenCentral()
@@ -14,6 +16,9 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("de.malkusch.whois-server-list:public-suffix-list:2.2.0")
 }
+
+lateinit var javadocJar: TaskProvider<Jar>
+lateinit var sourcesJar: TaskProvider<Jar>
 
 tasks {
     val download = register<de.undercouch.gradle.tasks.download.Download>("downloadPublicSuffixList") {
@@ -25,7 +30,7 @@ tasks {
     compileKotlin {
         kotlinOptions {
             jvmTarget = "1.8"
-            freeCompilerArgs = listOf("-XXLanguage:+InlineClasses")
+            freeCompilerArgs = freeCompilerArgs + "-Xinline-classes"
         }
         dependsOn(download)
     }
@@ -33,8 +38,45 @@ tasks {
     compileTestKotlin {
         kotlinOptions {
             jvmTarget = "1.8"
-            freeCompilerArgs = listOf("-XXLanguage:+InlineClasses")
+            freeCompilerArgs = freeCompilerArgs + "-Xinline-classes"
         }
         dependsOn(download)
+    }
+
+    // Include project license in generated JARs.
+    withType<Jar> {
+        from(project.projectDir) {
+            include("LICENSE")
+            into("META-INF")
+        }
+    }
+
+    // Generate Kotlin/Java documentation from sources.
+    dokka {
+        outputFormat = "html"
+    }
+
+    // JAR containing Kotlin/Java documentation.
+    javadocJar = register<Jar>("javadocJar") {
+        group = JavaBasePlugin.DOCUMENTATION_GROUP
+        dependsOn(dokka)
+        from(dokka)
+        archiveClassifier.set("javadoc")
+    }
+
+    // JAR containing all source files.
+    sourcesJar = register<Jar>("sourcesJar") {
+        from(sourceSets.main.get().allSource)
+        archiveClassifier.set("sources")
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            artifact(sourcesJar.get())
+            artifact(javadocJar.get())
+        }
     }
 }
